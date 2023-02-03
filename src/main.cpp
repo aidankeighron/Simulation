f38c47ab6e51c1cc76db73e1fbe44e993fc616a1
Aidan Keighron <aidankeighron@gmail.com>
2023-02-03 17:17:41 -0600

    launch fixed

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,20 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const unsigned int SCR_WIDTH = 1800;
+const unsigned int SCR_HEIGHT = 900;
 int main()
 {
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
+    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     // glfw window creation
     // --------------------
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,"LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Simulation", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -33,8 +34,8 @@ int main()
     // -----------
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.0f, 1.f, 1.0f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.3f, 0.3f, 0.3f,1.0f);
         // glfw: swap buffers and poll IO events (keypresses/released, mouse moved etc.)
         // ---------------------------------------------------
         glfwSwapBuffers(window);
