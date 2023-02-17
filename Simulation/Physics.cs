68b96971d3d4c6e894510789b71b0b412e3d84ab
Aidan Keighron <aidankeighron@gmail.com>
2023-02-17 12:14:18 -0600

    2d projectile motion

---
+++ Simulation/Physics.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Simulation
+{
+    class Physics
+    {
+        // Constants
+        public const float Gravity = 9.8f;
+        public const float Dt = Simulation.Interval / 1000.0f;
+
+        // Cube
+        private int CubeSize = 50;
+        private PointF CubeStartPosition = new PointF(150, 150);
+        private PointF CubePosition = new PointF(0, 0);
+        private PointF CubeStartVelocity = new PointF(0, 0);
+        private PointF CubeVelocity = new PointF(0, 0);
+
+        // 
+        private float ReferenceTimestamp = 0;
+        private int TextY = 0;
+
+        // Mouse
+        private PointF MousePosition = new PointF(0, 0);
+        private PointF LastMousePosition = new PointF(0, 0);
+        private PointF MouseVelocity = new PointF(0, 0);
+
+        public Physics()
+        {
+            Action<Graphics> drawPhysics = (g) =>
+            {
+                MouseControls();
+                HandleGravity();
+                BoundValues();
+                Pen p = new Pen(Color.Black, 1);
+                g.DrawRectangle(p, CubePosition.X - CubeSize / 2.0f, CubePosition.Y - CubeSize / 2.0f, CubeSize, CubeSize);
+                DisplayText("Position: ", CubePosition, g);
+                DisplayText("Position 0: ", CubeStartPosition, g);
+                DisplayText("Velocity: ", CubeVelocity, g);
+                DisplayText("Velocity 0: ", CubeStartVelocity, g);
+                DisplayText("MPos: ", MousePosition, g);
+                DisplayText("MVel 0: ", MouseVelocity, g);
+                TextY = 0;
+            };
+
+            Simulation.RegesterDrawer(drawPhysics);
+        }
+
+        public void HandleGravity()
+        {
+            CubePosition.X = CubeStartPosition.X + CubeStartVelocity.X * Timestamp();
+            CubePosition.Y = CubeStartPosition.Y + CubeStartVelocity.Y * Timestamp() + 0.5f * Gravity * Timestamp() * Timestamp();
+            CubeVelocity.X = CubeStartVelocity.X;
+            CubeVelocity.Y = CubeStartVelocity.Y + Gravity * Timestamp();
+        }
+
+        public void MouseControls()
+        {
+            MousePosition = Cursor.Position;
+            MouseVelocity = new PointF((MousePosition.X - LastMousePosition.X) / Dt, (MousePosition.Y - LastMousePosition.Y) / Dt);
+            LastMousePosition = MousePosition;
+            if (GetLeftMousePressed())
+            {
+                ReferenceTimestamp = Simulation.TimestampSeconds;
+                CubePosition = MousePosition;
+                CubeStartPosition = MousePosition;
+                CubeStartVelocity = MouseVelocity;
+                CubeVelocity = MouseVelocity;
+            }
+        }
+
+        public void BoundValues()
+        {
+            if (CubePosition.X <= 0 + CubeSize / 2.0f)
+            {
+                CubePosition.X = 0 + CubeSize / 2.0f;
+            }
+            if (CubePosition.X >= 1280 - CubeSize / 2.0f)
+            {
+                CubePosition.X = 1280 - CubeSize / 2.0f;
+            }
+            if (CubePosition.Y <= 0 + CubeSize / 2.0f)
+            {
+                CubePosition.Y = 0 + CubeSize / 2.0f;
+            }
+            if (CubePosition.Y >= 720 - CubeSize / 2.0f)
+            {
+                CubePosition.Y = 720 - CubeSize / 2.0f;
+            }
+        }
+
+        public float Timestamp()
+        {
+            return Simulation.TimestampSeconds - ReferenceTimestamp;
+        }
+
+        private void DisplayText(String text, object value, Graphics g)
+        {
+            g.DrawString(text + value.ToString(), new Font("Arial", 10), new SolidBrush(Color.Black), new PointF(5, 5 + TextY));
+            TextY += 20;
+        }
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(int VirtualKeyPressed);
+
+        public static bool GetLeftMousePressed()
+        {
+            if (GetAsyncKeyState(0x01) == 0)
+                return false;
+            else
+                return true;
+        }
+
+
+    }
+}
--- Simulation/Simulation.cs
+++ Simulation/Simulation.cs
@@ -1,34 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Simulation
 {
-    class Simulation
+    class Simulation : System.Windows.Forms.Form
     {
+        private static ArrayList Actions = new ArrayList();
+        public static float TimestampSeconds = 0;
+
+        // Constants
+        public const int Interval = 20;
+        // INIT //
+        public Simulation()
+        {
+            InitializeComponent();
+            Physics physics = new Physics();
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = Interval; // ms
+            timer.Tick += MainLoop;
+            timer.Start();
+        }
+
+        private void MainLoop(object sender, EventArgs e)
+        {
+            this.Refresh();
+            TimestampSeconds += Interval / 1000.0f;
+        }
+
+        private void InitializeComponent()
+        {
+            this.Text = "Simulation";
+            this.DoubleBuffered = true;
+            // this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintSimulation);
+        }
+
         static void Main(string[] args)
         {
-            Form myform = new Form();
-            Button mybutton = new Button()
-            {
-                Text = "Hello",
-                Location = new System.Drawing.Point(10, 10)
-            };
-            mybutton.Click += (o, s) =>
-            {
-                MessageBox.Show("world");
-            };
+            Application.Run(new Simulation());
+        }
 
-            myform.Controls.Add(mybutton);
-            myform.ShowDialog();
+        // INIT //
 
-            while (myform.Created)
+        private void PaintSimulation(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;// this.CreateGraphics();
+            foreach (Action<Graphics> action in Actions)
             {
-
+                action.Invoke(g);
             }
+        }
+
+        public static void RegesterDrawer(Action<Graphics> action)
+        {
+            Actions.Add(action);
+
         }
     }
 }
